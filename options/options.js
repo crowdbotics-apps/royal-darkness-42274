6fa1acdfe119ec92bb407fac9dbf94ae5977c79a
Crowdbotics <team@crowdbotics.com>
2023-07-19 12:47:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-darkness-42274",
+  url: "https://royal-darkness-42274.botics.co",
+  api: "https://royal-darkness-42274.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
